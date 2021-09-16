f8a3a7a462b04efc8d8c596aca3192bae2933af7
esun <esun@flexion.us>
2021-09-16 12:49:54 -0400

    add two-fer

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,13 +1,2 @@
 def convert(number):
-    result = ''
-    if number % 3 == 0:
-        result = 'Pling'
-    if number % 5 == 0:
-        result = result + 'Plang'
-    if number % 7 == 0:
-        result = result + 'Plong'
-    
-    if not result:
-        result = str(number)
-    
-    return result
+    pass
